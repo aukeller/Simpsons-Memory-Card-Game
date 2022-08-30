481dadc9edf93267b597e40b764fed99424764a2
Austin Keller <austinkllr@gmail.com>
2022-08-29 21:15:57 -0700

    finished static version of header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Simpsons Memory Game</h1>
+      <p>
+        Get points by clicking a character but don't click on any more than
+        once!
+      </p>
+    </div>
+  );
+};
+
+export default Header;
