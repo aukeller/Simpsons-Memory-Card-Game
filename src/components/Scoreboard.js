4f0fcabe24d845912f2ae8f7858aa4dd1c7c0ddd
Austin Keller <austinkllr@gmail.com>
2022-08-29 21:23:54 -0700

    finished static version of Scoreboard component

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Scoreboard = () => {
+  return (
+    <div>
+      <div>Score: </div>
+      <div>Best Score: </div>
+    </div>
+  );
+};
+
+export default Scoreboard;
