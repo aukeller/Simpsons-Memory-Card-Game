bb318f39b27302086c63b05f88cff513d567e086
Austin Keller <austinkllr@gmail.com>
2022-08-30 19:03:10 -0700

    created basic styling for App layout, makes header into flex

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import Header from "./components/Header";
 import Scoreboard from "./components/Scoreboard";
 import CardContainer from "./components/CardContainer";
 
+import styles from "./styles/App.module.css";
+
 function App() {
   return (
     <div>
-      <Header />
-      <Scoreboard />
+      <div className={styles.header}>
+        <Header />
+        <Scoreboard />
+      </div>
       <CardContainer />
     </div>
   );
