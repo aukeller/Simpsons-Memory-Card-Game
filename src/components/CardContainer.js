1f94e6e168033e44e90e7ba3a874f5ab63da2679
Austin Keller <austinkllr@gmail.com>
2022-08-30 19:22:55 -0700

    added styling to card container, arranged in a grid

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -2,6 +2,8 @@ import React from "react";
 import Card from "./Card";
 import images from "../assets";
 
+import styles from "../styles/CardContainer.module.css";
+
 const CardContainer = () => {
   const createCards = () => {
     const cards = [];
@@ -14,7 +16,7 @@ const CardContainer = () => {
     return cards;
   };
 
-  return <div>{createCards()}</div>;
+  return <div className={styles.container}>{createCards()}</div>;
 };
 
 export default CardContainer;
