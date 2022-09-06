792e4828c63639c6d5937a1b6990c9c8044d0ca6
Austin Keller <austinkllr@gmail.com>
2022-09-05 22:05:25 -0700

    finished basic styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
+import styles from "../styles/Card.module.css";
 
 const Card = (props) => {
+  const specialClass = useRef(false);
+
+  if (props.character === "Burns" || props.character === "Abe") {
+    specialClass.current = true;
+  }
+
   return (
-    <div>
-      <img src={props.image} alt={props.character} />
-      <span>{props.character}</span>
+    <div className={styles.card}>
+      <img
+        className={`${styles.image} ${
+          specialClass.current ? styles.special : ""
+        }`}
+        src={props.image}
+        alt={props.character}
+      />
+      <div className={styles.name}>
+        <span>{props.character}</span>
+      </div>
     </div>
   );
 };
