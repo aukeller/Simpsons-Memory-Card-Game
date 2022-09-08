c224eecbc8a720628e4c8706282a9b185d20e45b
Austin Keller <austinkllr@gmail.com>
2022-09-07 22:31:07 -0700

    added useEffect hook to cardContainer, so each card clicked shuffles container

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import images from "../assets";
 import _ from "lodash";
 import styles from "../styles/CardContainer.module.css";
 
 const CardContainer = (props) => {
-  const createCards = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
     const cards = [];
     for (let character in images) {
       let card = (
@@ -18,10 +20,10 @@ const CardContainer = (props) => {
       );
       cards.push(card);
     }
-    return _.shuffle(cards);
-  };
+    setCards(_.shuffle(cards));
+  }, [props.cardClick]);
 
-  return <div className={styles.container}>{createCards()}</div>;
+  return <div className={styles.container}>{cards}</div>;
 };
 
 export default CardContainer;
