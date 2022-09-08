6ce48e3c914c59b824ed4c01e97d7b12c377d2dc
Austin Keller <austinkllr@gmail.com>
2022-09-07 21:26:07 -0700

    added lodash shuffle function and added to create card function to randomly place cards when called

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,19 +1,24 @@
 import React from "react";
 import Card from "./Card";
 import images from "../assets";
-
+import _ from "lodash";
 import styles from "../styles/CardContainer.module.css";
 
-const CardContainer = () => {
+const CardContainer = (props) => {
   const createCards = () => {
     const cards = [];
     for (let character in images) {
       let card = (
-        <Card key={character} character={character} image={images[character]} />
+        <Card
+          cardClick={props.cardClick}
+          key={character}
+          character={character}
+          image={images[character]}
+        />
       );
       cards.push(card);
     }
-    return cards;
+    return _.shuffle(cards);
   };
 
   return <div className={styles.container}>{createCards()}</div>;
