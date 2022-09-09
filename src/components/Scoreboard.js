6b0065284ec935662391c364251695e56c18fa27
Austin Keller <austinkllr@gmail.com>
2022-09-08 21:55:11 -0700

    completed scoreboard, which displays highscore after comparing score in props with useeffect

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Scoreboard = (props) => {
+  const [highScore, setHighScore] = useState(0);
+
+  useEffect(() => {
+    if (props.score > highScore) {
+      setHighScore(props.score);
+    }
+  }, [props.score, highScore]);
+
   return (
     <div>
       <div>Score: {props.score}</div>
-      <div>Best Score: 0 </div>
+      <div>Best Score: {highScore} </div>
     </div>
   );
 };
